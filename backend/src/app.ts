8bec4aaa16f4973baf9c25bec83bbec4793e3185
doctorbond2 <michael.rostlund@hotmail.com>
2024-03-26 21:21:21 +0100

    asd

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,12 +8,19 @@ const ALLOWED_ORIGINS: string[] = [
 ];
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
+app.use();
+app.use(function (req, res, next) {
   cors({
-    origin: "*",
-  })
-);
-
+    origin: ALLOWED_ORIGINS,
+  });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
 app.use("/api/v1/users", userRouter);
 app.use("/", (req, res) => {
   res.send("Server is up and running!!" + process.env.PORT);
