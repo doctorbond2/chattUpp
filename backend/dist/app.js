821eb54b5096e38ee6b711c7b69968bfb85c6495
doctorbond2 <michael.rostlund@hotmail.com>
2024-03-26 21:40:11 +0100

    cors again....

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -10,10 +10,15 @@ const ALLOWED_ORIGINS = [
 ];
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use();
-app.use(cors({
-    origin: ALLOWED_ORIGINS,
-}));
+app.use(function (req, res, next) {
+    cors({
+        origin: ALLOWED_ORIGINS,
+    });
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
 app.use("/api/v1/users", userRouter);
 app.use("/", (req, res) => {
     res.send("Server is up and running!!" + process.env.PORT);
