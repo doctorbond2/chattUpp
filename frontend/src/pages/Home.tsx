16b811be8d4c6942372704668f8b8c4901b86bd5
doctorbond2 <michael.rostlund@hotmail.com>
2024-03-26 20:56:38 +0100

    testing connection

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { ActiveUser } from "../types/userTypes";
 type Props = { loggedIn: ActiveUser | null };
 const Home: React.FC<Props> = ({}) => {
+  const [test, setTest] = useState<any>(null);
   const BaseUrl = import.meta.env.VITE_BaseUrl;
   console.log(BaseUrl);
   useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(BaseUrl + "/");
+      if (response) {
+        setTest(response.data);
+      }
+      {
+        setTest("Response error");
+      }
+    };
+    fetchData();
     return () => {
       console.log("Homepage cleanup");
     };
@@ -13,6 +25,7 @@ const Home: React.FC<Props> = ({}) => {
   return (
     <>
       <div>Homepage</div>
+      <h2>{test}</h2>
     </>
   );
 };
