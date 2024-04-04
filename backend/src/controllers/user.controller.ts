ca1b939ddeca5816c495b2923892a8e4ca93c971
doctorbond2 <michael.rostlund@hotmail.com>
2024-04-04 09:30:05 +0200

    juh

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -88,3 +88,21 @@ export const getUserProfile = async (req: Request, res: Response) => {
     });
   }
 };
+const getUserList = async (req: Request, res: Response) => {
+  if (!req.query.page || !req.query.pageSize) {
+    return res.status(500);
+  }
+  let page: number = parseInt(req.query.page as string) || 1;
+  let pageSize: number = parseInt(req.query.pageSize as string) || 10;
+  let pageSkip = page - 1;
+  try {
+    let _users = User.find().skip(pageSkip).limit(pageSize);
+    res.status(200).json(_users);
+  } catch (err: any) {
+    console.error(err.message);
+    res.status(500).json({
+      message: "Unexpected error.",
+      error: err.message,
+    });
+  }
+};
--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -9,6 +9,7 @@ import {
 router.post("/create", createUser);
 router.post("/login", loginUser);
 router.get("/profile/:id", getUserProfile);
+router.get("/user/list");
 router.post("/update/:id");
 
 export default router;
