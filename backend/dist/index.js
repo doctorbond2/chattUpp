740cc96f2502c566aa16df0f26f15d3d07b1e21c
doctorbond2 <michael.rostlund@hotmail.com>
2024-09-22 18:35:41 +0200

    Refactor database configuration in index.js and index.ts

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,5 +1,8 @@
-import { RUN_SERVER } from "./config/dbConfig.js";
+import { RUN_SERVER } from './config/dbConfig.js';
+const isProduction = process.env.NODE_ENV === 'production';
 const PORT = process.env.PORT || 3000;
-const DATABASE_PATH = process.env.DATABASE_PATH || "undefined";
+const DATABASE_PATH = isProduction
+    ? process.env.MONGODB_URI || 'default_production_db_path'
+    : process.env.DATABASE_PATH || 'default_development_db_path';
 console.log(PORT);
 RUN_SERVER(DATABASE_PATH, PORT);
